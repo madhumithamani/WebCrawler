28513fc5178ee3efb00d2d37702f5c4d5e7bc0b2
madhumithamani <madhumitha.mani@gmail.com>
2016-04-26 14:27:39 -0700

    Modified to include "title" column and add links to the pages instead of page titles

--- src/main/java/createReport.java
+++ src/main/java/createReport.java
@@ -1,6 +1,7 @@
 import java.awt.Desktop;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import org.apache.commons.io.FileUtils;
 import org.jsoup.Jsoup;
@@ -16,7 +17,7 @@ import org.jsoup.nodes.Element;
  */
 public class createReport {
 
-	public void createHTML(PageInfo[] allPages) {
+	public void createHTML(ArrayList<PageInfo> allPages) {
 
 		File htmlTemplateFile = new File("src/main/java/template.html");
 		String htmlString = null;
@@ -39,16 +40,21 @@ public class createReport {
 						.last()
 						.append("<td> <a href = " + pageObj.getliveURL() 
 								+ " target = '_blank' >"
-								+ pageObj.getTitle()
+								+ pageObj.getliveURL()
+								//+ pageObj.getTitle()
 								+ "</td>");
 				tableElement
 						.select("tr")
 						.last()
 						.append("<td> <a href = " + pageObj.getlocalURL()
-								+ " target = '_blank' >" 
-								+ pageObj.getTitle()
+								+ " target = '_blank' >"
+								+ pageObj.getlocalURL()
+								//+ pageObj.getTitle()
 								+ "</td>");
-				
+
+				tableElement.select("tr").last()
+						.append("<td>" + pageObj.getTitle() + "</td>");
+
 				tableElement.select("tr").last()
 						.append("<td>" + pageObj.getHttpStatusCode() + "</td>");
 				
@@ -72,31 +78,31 @@ public class createReport {
 		}
 	}
 
-	public static void main(String[] args) {
-		createReport myHtml = new createReport();
-		PageInfo page1 = new PageInfo(
-				"https://phonebook.scu.edu/",
-				"Santa Clara University",
-				8,
-				118,
-				200,
-				"C:/Users/Nisha/Documents/WebCrawlerRepository/9af5cf98-bbde-4c36-9b29-770ae4704ec8-479181225.html");
-		PageInfo page2 = new PageInfo(
-				"https://www.linkedin.com/edu/santa-clara-university-17914",
-				"Santa Clara University | LinkedIn",
-				40,
-				250,
-				200,
-				"C:/Users/Nisha/Documents/WebCrawlerRepository/6e00754e-a842-4016-b366-3a6f70360cc7--2135172176.html");
-		PageInfo page3 = new PageInfo(
-				"https://magazine.scu.edu/webonly.cfm?b=439&c=23875",
-				"Santa Clara Magazine - SCM Web Only",
-				4,
-				69,
-				200,
-				"C:/Users/Nisha/Documents/WebCrawlerRepository/ac8ef28f-7db5-49bf-a4af-e9b2c10bba9a--285624058.html");
-		PageInfo[] allPages = { page1, page2, page3 };
-		myHtml.createHTML(allPages);
-	}
+//	public static void main(String[] args) {
+//		createReport myHtml = new createReport();
+//		PageInfo page1 = new PageInfo(
+//				"https://phonebook.scu.edu/",
+//				"Santa Clara University",
+//				8,
+//				118,
+//				200,
+//				"C:/Users/Nisha/Documents/WebCrawlerRepository/9af5cf98-bbde-4c36-9b29-770ae4704ec8-479181225.html");
+//		PageInfo page2 = new PageInfo(
+//				"https://www.linkedin.com/edu/santa-clara-university-17914",
+//				"Santa Clara University | LinkedIn",
+//				40,
+//				250,
+//				200,
+//				"C:/Users/Nisha/Documents/WebCrawlerRepository/6e00754e-a842-4016-b366-3a6f70360cc7--2135172176.html");
+//		PageInfo page3 = new PageInfo(
+//				"https://magazine.scu.edu/webonly.cfm?b=439&c=23875",
+//				"Santa Clara Magazine - SCM Web Only",
+//				4,
+//				69,
+//				200,
+//				"C:/Users/Nisha/Documents/WebCrawlerRepository/ac8ef28f-7db5-49bf-a4af-e9b2c10bba9a--285624058.html");
+//		PageInfo[] allPages = { page1, page2, page3 };
+//		myHtml.createHTML(allPages);
+//	}
 
 }
