70aa17c23243df930bcca9d23a7081072ebfc9d0
madhumithamani <madhumitha.mani@gmail.com>
2016-04-26 14:21:59 -0700

    Added code to include DensitySum and calculate new threshold based on maxDensitySum. Also, it removes any children if <a href> tag is the parent.

--- src/main/java/DOMParser.java
+++ src/main/java/DOMParser.java
@@ -1,29 +1,35 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Node;
 import org.jsoup.select.Elements;
 import org.jsoup.nodes.Element;
-import org.jsoup.select.NodeVisitor;
-import org.jsoup.select.Selector;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.*;
 
 public class DOMParser {
 
     private static StringBuilder stringBuilder = new StringBuilder();
     private static StringBuilder htmlBuilder = new StringBuilder();
 
-    private static void readHTMLFile(){
+    public static void readAllFiles() {
+//        File folder = new File("/Users/MadVish/Documents/WSIR/coen272/projectsamples/repository/");
+//        File[] filesList = folder.listFiles();
+//        for (File file : filesList) {
+//            if (file.isFile() && !file.getName().equalsIgnoreCase(".DS_Store")) {
+//                System.out.println(file.getName());
+//                System.out.println(file.getAbsolutePath());
+//                stringBuilder.setLength(0);
+//                htmlBuilder.setLength(0);
+//                readHTMLFile(file.getAbsolutePath());
+//            }
+//        }
+        readHTMLFile("/Users/MadVish/Documents/ttr.html");
+    }
+
+    private static void readHTMLFile(String filePath){
         FileReader fileReader = null;
         try {
-
             String line;
-            fileReader = new FileReader("/Users/MadVish/Documents/ttr.html");
+            fileReader = new FileReader(filePath);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
             while((line = bufferedReader.readLine())!=null){
                 line = line.trim();
@@ -32,6 +38,7 @@ public class DOMParser {
                 }
             }
             bufferedReader.close();
+            preprocess();
             //System.out.println(stringBuilder.toString());
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -50,10 +57,14 @@ public class DOMParser {
 
     private static void preprocess(){
         Document document = Jsoup.parse(stringBuilder.toString());
-        document.select("script").remove();
-        document.select("style").remove();
-        document.select("header").remove();
-        document.select("meta").remove();
+        System.out.println(document.charset());
+//        document.select("script").remove();
+//        document.select("style").remove();
+//        document.select("header").remove();
+//        document.select("meta").remove();
+        document.select("br").remove();
+
+        document.select("script, style, header, footer, nav, meta, comment, CDATA, #comment").remove();
         traverse(document);
     }
 
@@ -63,20 +74,63 @@ public class DOMParser {
         int LCb = Integer.parseInt(document.body().attr("numOfLinkChar"));
         calculateCompositeTextDensity(document.body().getAllElements().first(), Cb, LCb);
         double threshold = Double.parseDouble(document.body().attr("ctd"));
-        //System.out.println(document.body().getAllElements().first());
+        System.out.println(Integer.parseInt(document.body().attr("numoflinks")));
+        System.out.println(Integer.parseInt(document.body().attr("numoflinkchar")));
+        System.out.println(Integer.parseInt(document.body().attr("numoftags")));
+        System.out.println(Integer.parseInt(document.body().attr("numofchar")));
+
+        System.out.println(document.body().getAllElements().first());
         System.out.println("Threshold:" + threshold);
-        String text = extractText(document.body().getAllElements().first(), threshold);
+        //String text = extractText(document.body().getAllElements().first(), threshold);
+//        Element maxDensitySumElem = findMaxDensitySumNode(document.body());
+
+        Element maxDensitySumElem = findMaxCTDDensitySumNode(document.body());
+        Double maxDensitySum = Double.parseDouble(maxDensitySumElem.attr("ctd_densitysum"));
+        System.out.println("Max Density Sum:" + maxDensitySum);
+        //System.out.println("Max Density Sum Elem" + maxDensitySumElem.text());
+        threshold = printCTDToBody(maxDensitySumElem, document.body());
+        System.out.println("New T : " + threshold);
+        String text = extractText(document.body().getAllElements().first(), threshold,maxDensitySum);
+        System.out.println(text);
+
+//        System.out.println(maxDensitySumElem.text());
         //System.out.println(text);
         htmlBuilder.append("</body></html>");
         Document doc = Jsoup.parse(htmlBuilder.toString());
         System.out.println(doc);
-        postProcess(doc);
+//        Whitelist whitelist = Whitelist.simpleText();
+       // postProcess(doc,threshold);
+
     }
 
-    private static void postProcess(Document doc){
-        doc.select("a[href]").remove();
+    private static void postProcess(Document doc, double threshold){
+        Elements elements= doc.select("a[href]");
+        if(elements!=null) {
+            for (Element element : elements) {
+                if (element.children().size() > 0) {
+                    Elements elements1 = element.getAllElements();
+                    for (Element child : elements1) {
+                        if(!(Double.parseDouble(child.attr("ctd")) == 0.0))
+                            child.remove();
+                    }
+                } else {
+                    if (!(Double.parseDouble(element.attr("ctd")) == 0.0))
+                        element.remove();
+                }
+            }
+        }
+        //doc.select("a[href]").remove();
         doc.select("img").remove();
-        String text = extractText(doc.body().getAllElements().first(),0.0);
+//        System.out.println(doc);
+        String text = extractText(doc.body().getAllElements().first(),threshold);
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("/Users/MadVish/Documents/WSIR/coen272/projectsamples/output.txt", true));
+            writer.append(text);
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         System.out.println(text);
     }
 
@@ -102,7 +156,6 @@ public class DOMParser {
         if(node == null) {
             return;
         }
-        int children = node.children().size();
         for(Element element : node.children()){
             computeDensity(element);
         }
@@ -123,28 +176,48 @@ public class DOMParser {
         }
 
         elements = node.children();
-        int tagCount =0 ;
+
+
         if(elements!=null && !elements.isEmpty()){
+            int tagCount =0 ;
+            int densitySum = 0;
             for(Element element : elements){
+                densitySum += Integer.parseInt(element.attr("textDensity"));
                 tagCount+=Integer.parseInt(element.attr("numOfTags"));
                 tagCount += 1;
             }
             node.attr("numOfTags", String.valueOf(tagCount));
             node.attr("textDensity", String.valueOf(node.text().length()/tagCount));
+            node.attr("densitySum", String.valueOf(densitySum));
         }else{
             node.attr("textDensity", String.valueOf(node.text().length()));
             node.attr("numOfTags", "0");
+            node.attr("densitySum", node.attr("textDensity"));
         }
         node.attr("numOfChar", String.valueOf(node.text().length()));
     }
-
+//
+//    private static void calculateDensitySum(final Element node){
+//        for(Element element : node.children()) {
+//            if (element.children() != null && !element.children().isEmpty()) {
+//                calculateDensitySum(element);
+//                if (!returnTxt.trim().isEmpty()) {
+//                    stringBuilder.append(returnTxt);
+//                    //htmlBuilder.append(element);
+//                }
+//            }
+//        }
+//    }
     private static void calculateCompositeTextDensity(Element node,int Cb,int LCb){
+        String CTDDensitySumStr = "CTD_DensitySum";
         if(node==null)
             return;
+        double CTD_densitySum = 0D;
         for(Element element : node.children()){
             calculateCompositeTextDensity(element,Cb,LCb);
+            CTD_densitySum += Double.parseDouble(element.attr("CTD"));
         }
-
+        node.attr(CTDDensitySumStr, String.valueOf(CTD_densitySum));
         int C = Integer.parseInt(node.attr("numOfChar"));
         int T = Integer.parseInt(node.attr("numOfTags"));
         int LC = Integer.parseInt(node.attr("numOfLinkChar"));
@@ -155,24 +228,66 @@ public class DOMParser {
         T = (T==0)?1:T;
         LC = (LC==0)?1:LC;
         LT = (LT==0)?1:LT;
-//        nonLC = (nonLC==0)?1:nonLC;
+        //nonLC = (nonLC==0)?1:nonLC;
 
        double result= Math.round(C/T * Math.log(C/LC * T/LT));
-//        double numerator = first * (Math.log(C/LC * T/LT));
-//        first = Math.log(((C/nonLC) * LC) + (LCb/Cb)*C);
-//        double denominator = Math.round(Math.log(first));
-//        denominator = (denominator==0)?1:denominator;
+
+//
+//        double numerator = (C/T) * (Math.log(C/LC * T/LT));
+//        double first = Math.log(((C/nonLC) * LC) + (LCb/Cb)*C + 10);
+//        double denominator = Math.log(first);
+//        denominator = (denominator<=0)?1:denominator;
 //        double result = numerator/denominator;
 //        System.out.println(result);
+
         if(result>0)
             node.attr("CTD",String.valueOf(result));
         else
             node.attr("CTD","0");
     }
 
-    private static String extractText(final Element node, final double threshold){
-        StringBuilder stringBuilder = new StringBuilder();
+    private static Element findMaxDensitySumNode(final Element node){
+        Element returnElement = node;
+        for (Element element : node.children()) {
+            Element maxNode = findMaxDensitySumNode(element);
+            int maxNodeDensitySum = Integer.parseInt(maxNode.attr("densitySum"));
+            int returnElementDensitySum = Integer.parseInt(returnElement.attr("densitySum"));
+            if(maxNodeDensitySum > returnElementDensitySum){
+                returnElement = maxNode;
+            }
+        }
+        return returnElement;
+    }
+    private static double printCTDToBody(Element node, final Element body){
+        Element returnNode = node;
+        while(node != body){
+            System.out.println(node.tagName() + " ::: CTD:" + node.attr("CTD") + ", CTD_densitySum:" + node.attr("CTD_DensitySum") + ", densitySum:" + node.attr("densitySum"));
+            if(Double.parseDouble(node.attr("CTD")) < Double.parseDouble(returnNode.attr("CTD"))){
+                returnNode = node;
+            }
+            node = node.parent();
 
+        }
+        System.out.println("New threshold:" + returnNode.attr("CTD"));
+        return Double.parseDouble(returnNode.attr("CTD"));
+        //return Double.parseDouble(returnNode.attr("CTD_densitySum"));
+    }
+    private static Element findMaxCTDDensitySumNode(final Element node){
+        Element returnElement = node;
+        for (Element element : node.children()) {
+            Element maxNode = findMaxCTDDensitySumNode(element);
+            double maxNodeDensitySum = Double.parseDouble(maxNode.attr("CTD_DensitySum"));
+            double returnElementDensitySum = Double.parseDouble(returnElement.attr("CTD_DensitySum"));
+            if(maxNodeDensitySum > returnElementDensitySum){
+                returnElement = maxNode;
+            }
+        }
+        return returnElement;
+    }
+
+    private static String extractText(final Element node, final double threshold){
+
+        StringBuilder stringBuilder = new StringBuilder();
         htmlBuilder.append("<html><body>");
         for(Element element : node.children()){
             if(Double.parseDouble(element.attr("ctd")) >= threshold){
@@ -183,7 +298,7 @@ public class DOMParser {
                     htmlBuilder.append(element);
                 }
 
-            }
+           }
             if(element.children() != null && !element.children().isEmpty()){
                 String returnTxt = extractText(element, threshold);
                 if(!returnTxt.trim().isEmpty()){
@@ -196,8 +311,56 @@ public class DOMParser {
         return stringBuilder.toString();
     }
 
+    private static String extractText(final Element node, final double threshold,double maxDensitySum){
+        StringBuilder stringBuilder = new StringBuilder();
+        htmlBuilder.append("<html><body>");
+        for(Element element : node.children()) {
+            System.out.println("DensitySum:" + element.attr("ctd_densitysum"));
+            if (Double.parseDouble(element.attr("ctd_densitysum")) == maxDensitySum) {
+                stringBuilder.append(element.text());
+                element.children().remove();
+                //System.out.println("hello");
+            }
+            else if(Double.parseDouble(element.attr("ctd")) >= threshold && element.parent().attr("href").length()==0) {
+                    String elemText = element.ownText().trim();
+                    if (!elemText.isEmpty()) {
+                        //TODO:Remove the \n if needed.
+                        stringBuilder.append(elemText).append("\n");
+                        htmlBuilder.append(element);
+                    }
+                }
+
+            if (element.children() != null && !element.children().isEmpty()) {
+                    String returnTxt = extractText(element, threshold,maxDensitySum);
+                if (!returnTxt.trim().isEmpty()) {
+                    stringBuilder.append(returnTxt);
+                }
+
+ }
+            //else {
+//                if (Double.parseDouble(element.attr("ctd")) >= threshold) {
+//                    String elemText = element.ownText().trim();
+//                    if (!elemText.isEmpty()) {
+//                        //TODO:Remove the \n if needed.
+//                        stringBuilder.append(elemText).append("\n");
+//                        htmlBuilder.append(element);
+//                    }
+//
+//                }
+//                if (element.children() != null && !element.children().isEmpty()) {
+//                    String returnTxt = extractText(element, threshold);
+//                    if (!returnTxt.trim().isEmpty()) {
+//                        stringBuilder.append(returnTxt);
+//                        //htmlBuilder.append(element);
+//                    }
+//                }
+//            }
+        }
+
+        return stringBuilder.toString();
+    }
     public static void main(String[] args) {
-        DOMParser.readHTMLFile();
-        DOMParser.preprocess();
+        DOMParser.readAllFiles();
+       // DOMParser.preprocess();
     }
 }
